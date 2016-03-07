c6369e07a7577c08aa7872faf49cad2cd4a7c833
Flyear <flyear@flyear.net>
2016-03-08 01:05:09 +0800

    1. Added Publish Profile 2. Enable Cors

---
+++ src/MStack.MainSite/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Cors;
+
+[assembly: OwinStartup(typeof(MStack.MainSite.Startup))]
+
+namespace MStack.MainSite
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCors(CorsOptions.AllowAll);
+        }
+    }
+}
