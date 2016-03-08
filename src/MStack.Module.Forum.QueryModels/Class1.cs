9777eb1fc1ff5e03aefdcd2e985478062f62d52e
Flyear <flyear@flyear.net>
2016-03-08 22:43:53 +0800

    增加基本分层

---
+++ src/MStack.Module.Forum.QueryModels/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MStack.Module.Forum.QueryModels
+{
+    public class Class1
+    {
+    }
+}
