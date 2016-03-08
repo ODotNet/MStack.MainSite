cf45f07d0dd0714f3ff47caa70187da6f3b29c1b
Flyear <flyear@flyear.net>
2016-03-08 22:25:43 +0800

    1. 引入tether as bootstrap 4 required 2., 添加论坛相关action

--- src/MStack.MainSite/Controllers/ForumController.cs
+++ src/MStack.MainSite/Controllers/ForumController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MStack.MainSite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,63 @@ namespace MStack.MainSite.Controllers
 {
     public class ForumController : Controller
     {
-        // GET: Forum
+        /// <summary>
+        /// 论坛模块首页
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 帖子列表部分页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult TopicList()
+        {
+            var viewModel = new List<Topic>();
+            return PartialView(viewModel);
+        }
+
+        /// <summary>
+        /// 发帖页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Publish()
+        {
+            var viewModel = new Topic();
+            return View(viewModel);
+        }
+
+        [HttpPost, ActionName("Publish")]
+        public ActionResult PublishPost()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 编辑Topic页面
+        /// </summary>
+        /// <param name="topicId"></param>
+        /// <returns></returns>
+        public ActionResult EditTopic(Guid topicId)
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 编辑Topic提交Action
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public ActionResult EditTopicPost(Topic model)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Comment(Comment comment)
         {
             return View();
         }
---
+++ src/MStack.MainSite/Models/ForumModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MStack.MainSite.Models
+{
+    /// <summary>
+    /// 论坛主帖
+    /// </summary>
+    public class Topic
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishDateTime { get; set; }
+
+        public Author Author { get; set; }
+    }
+
+    public class Author
+    {
+        public Guid AuthorId { get; set; }
+        public string AuthorName { get; set; }
+    }
+
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CommentDateTime { get; set; }
+        public Author Author { get; set; }
+    }
+}
