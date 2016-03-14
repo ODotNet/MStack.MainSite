41dff639dcec9174d20f4f0cfaaa53e312354f43
DukeCheng <flyear@flyear.net>
2016-03-14 21:20:40 +0800

    增加LinkedIn, GitHub帐号登录支持

--- src/MStack.MainSite/Startup.cs
+++ src/MStack.MainSite/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Owin.Cors;
 using MStack.MainSite.WebFramework.Authentication;
 using Microsoft.AspNet.Identity;
 using MStack.MainSite.Controllers;
+using KatanaContrib.Security.Github;
+using KatanaContrib.Security.LinkedIn;
 
 [assembly: OwinStartup(typeof(MStack.MainSite.Startup))]
 
@@ -27,26 +29,35 @@ namespace MStack.MainSite
             //{
             //    AppId = "101294257",
             //    AppSecret = "10ef6f7c0f935d892c058f240474645d",
-            //    //Caption = "QQ登录"
+            //    ReturnEndpointPath = "/signin-qqconnect",
+            //    Caption = "QQ登录"
             //    //SignInAsAuthenticationType = "QQ登录",
             //});
             app.UseQQConnectAuthentication(appId: "101294257", appSecret: "10ef6f7c0f935d892c058f240474645d");
-
-
-            //app.UseWeChatAuthentication(new Microsoft.Owin.Security.WeChat.WeChatAuthenticationOptions()
-            //{
-            //    AppId = "101294257",
-            //    AppSecret = "10ef6f7c0f935d892c058f240474645d",
-            //    Caption = "微信登录",
-            //    SignInAsAuthenticationType = "微信登录"
-            //});
 
             app.UseMicrosoftAccountAuthentication(new Microsoft.Owin.Security.MicrosoftAccount.MicrosoftAccountAuthenticationOptions
             {
                 ClientId = "000000004018CC02",
                 ClientSecret = "r-qtEQMR8-PIAWc9Zlfp2unOggVUiCuH",
+                CallbackPath = new PathString("/signin-microsoft"),
                 Caption = "Windows帐号登录",
-                SignInAsAuthenticationType = "Windows帐号登录"
+
+            });
+
+            app.UseGithubAuthentication(new GithubAuthenticationOptions()
+            {
+                ClientId = "1867a5f61a2201829162",
+                ClientSecret = "95a1b9c9e10b8d614f3416f308d49d7466e0ec8e",
+                CallbackPath = new PathString("/signin-github"),
+                Caption = "GitHub"
+            });
+
+            app.UseLinkedInAuthentication(new LinkedInAuthenticationOptions()
+            {
+                AppId = "759bsrm6x5i6er",
+                AppSecret = "o9Ht8ktjw6tHhoXm",
+                CallbackPath = new PathString("/signin-linkedin"),
+                Caption = "LinkedIn(领英)",
             });
         }
     }
