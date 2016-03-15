40c38557f2abcce45cb9c6ddb1c580f599455a6e
RenQi <ren8179@126.com>
2016-03-15 23:32:53 +0800

    帖子增加分类，回复，查看，修改相关功能及页面UI

--- src/MStack.Infrastructure.Entities/Topic.cs
+++ src/MStack.Infrastructure.Entities/Topic.cs
@@ -17,5 +17,18 @@ namespace MStack.Infrastructure.Entities
         public DateTime PublishDateTime { get; set; }
         [DisplayName("作者")]
         public User Author { get; set; }
+        [DisplayName("分类")]
+        public EnumCategory Category { get; set; }
+        [DisplayName("回复")]
+        public int Replies { get; set; }
+        [DisplayName("查看")]
+        public int Views { get; set; }
+    }
+
+    public enum EnumCategory
+    {
+        DoNet = 0,
+        UI =1,
+        Other=2
     }
 }
