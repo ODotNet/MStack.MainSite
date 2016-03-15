193f40a2b3d9434df76df2a1e6b98b791993fcc7
oBears <xv_chang@126.com>
2016-03-16 01:04:44 +0800

    用户基础信息编辑，后续优化 添加了ajaxfileupload异步上传插件 添加了jcrop图像裁剪插件

---
+++ src/MStack.MainSite/Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MStack.MainSite.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [System.Web.Mvc.HiddenInput]
+        public Guid Id { set; get; }
+        [ReadOnly(true)]
+        [Display(Name ="账号")]
+        public string UserName { get; set; }
+        [ReadOnly(true)]
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "昵称")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        public string DisplayName { get; set; }
+        [Display(Name = "头像")]
+        public string Avatar { set; get; }
+        [Display(Name = "公司")]
+        public string Company { set; get; }
+        [System.Web.Mvc.HiddenInput]
+        public string AvatarState { set; get; }
+    }
+    
+}
